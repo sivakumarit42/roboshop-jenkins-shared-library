39d5b4845e2dd037d4efb6050e7b2ca7ba6b9470
DELL <sivakumarit42@gmail.com>
2023-04-14 09:36:47 +0530

    changes

--- vars/common.groovy
+++ vars/common.groovy
@@ -28,7 +28,7 @@ def codequality() {
 def prepareArtifacts() {
     sh 'echo ${TAG_NAME} >VERSION'
     if (app_lang == "nodejs" || app_lang == "angular") {
-        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile_d'
     }
 
 }
